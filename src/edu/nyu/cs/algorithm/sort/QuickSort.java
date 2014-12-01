88c689059fd161f8522566cb40f54c6774b2b51d
shenli <sl3268@nyu.edu>
2014-11-30 20:12:46 -0800

    Add quick sort algorithm implementation & unit tests

---
+++ src/edu/nyu/cs/algorithm/sort/QuickSort.java
@@ -0,0 +1,63 @@
+package edu.nyu.cs.algorithm.sort;
+
+import edu.nyu.cs.algorithm.util.Utility;
+
+public class QuickSort {
+    
+    private QuickSort() {
+        throw new AssertionError();
+    }
+    
+    public static void sort(byte[] a) {
+        sortHelper(a, 0, a.length - 1);
+    }
+    
+    private static void sortHelper(byte[] a, int low, int high) {
+        int q = 0;
+        if (low < high) {
+            q = partition(a, low, high);
+            sortHelper(a, low, q - 1);
+            sortHelper(a, q + 1, high);
+        }
+    }
+    
+    private static int partition(byte[] a, int low, int high) {
+        byte key = a[high];
+        int j = low - 1;
+        for (int i = low; i < high; i++) {
+            if (a[i] <= key) {
+                j = j + 1;
+                Utility.exchange(a, i, j);
+            }
+        }
+        Utility.exchange(a, j + 1, high);
+        return j + 1;
+    }
+    
+    public static <T extends Comparable<? super T>> void sort(T[] a) {
+        sortHelper(a, 0, a.length - 1);
+    }
+    
+    private static <T extends Comparable<? super T>> void sortHelper(T[] a, int low, int high) {
+        int q = 0;
+        if (low < high) {
+            q = partition(a, low, high);
+            sortHelper(a, low, q - 1);
+            sortHelper(a, q + 1, high);
+        }
+    }
+    
+    private static <T extends Comparable<? super T>> int partition(T[] a, int low, int high) {
+        T key = a[high];
+        int j = low - 1;
+        for (int i = low; i < high; i++) {
+            if (a[i].compareTo(key) <= 0) {
+                j = j + 1;
+                Utility.exchange(a, i, j);
+            }
+        }
+        Utility.exchange(a, j + 1, high);
+        return j + 1;
+    }
+
+}
---
+++ tst/edu/nyu/cs/algorithm/sort/QuickSortTest.java
@@ -0,0 +1,137 @@
+package edu.nyu.cs.algorithm.sort;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class QuickSortTest {
+    private final byte[] byteAfterSorting =
+        { 2,  5,  7,  7,  7,   7,  10,  10,  23,
+         31, 31, 40, 41, 55,  55,  55,  62,  64,
+         70, 88, 89, 90, 94, 101, 105, 126, 126};
+    private final String[] objectAfterSorting = 
+        {    "amy",  "berny", "eleven",  "eleven",
+          "eleven", "howard", "howard", "leonard",
+            "lucy",  "penny", "rajesh", "sheldon",
+         "sheldon", "stuart",   "zack",    "zack"};
+
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(byte[])}.
+     */
+    @Test
+    public void testSortByteArrayWithSingleElement() {
+        byte[] a = {9};
+        byte[] b = {9};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(b[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(byte[])}.
+     */
+    @Test
+    public void testSortByteArrayWithIncreaseOrderElements() {
+        byte[] a = 
+            { 2,  5,  7,  7,  7,   7,  10,  10,  23,
+             31, 31, 40, 41, 55,  55,  55,  62,  64,
+             70, 88, 89, 90, 94, 101, 105, 126, 126};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(byteAfterSorting[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(byte[])}.
+     */
+    @Test
+    public void testSortByteArrayWithDecreaseOrderElements() {
+        byte[] a = 
+            {126, 126, 105, 101, 94, 90, 89, 88, 70,
+              64,  62,  55,  55, 55, 41, 40, 31, 31,
+              23,  10,  10,   7,  7,  7,  7,  5,  2};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(byteAfterSorting[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(byte[])}.
+     */
+    @Test
+    public void testSortByteArrayWithRandomOrderElements() {
+        byte[] a =
+            {41, 101,  70,  7,  7, 23, 55, 64, 126,
+             55,   7,   2, 90, 89, 31, 40, 10,  31,
+             94, 105, 126, 88, 55, 10,  5,  7,  62};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(byteAfterSorting[i], a[i]);
+        }
+    }
+
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(T[])}.
+     */
+    @Test
+    public void testSortTArrayWithSingleElement() {
+        String[] a = {"amy"};
+        String[] b = {"amy"};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(b[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithms.sort.QuickSort#sort(T[])}.
+     */
+    @Test
+    public void testSortTArrayWithIncreaseOrderElements() {
+        String[] a = 
+            {   "amy",  "berny", "eleven",  "eleven",
+             "eleven", "howard", "howard", "leonard",
+               "lucy",  "penny", "rajesh", "sheldon",
+            "sheldon", "stuart",   "zack",    "zack"};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(objectAfterSorting[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(T[])}.
+     */
+    @Test
+    public void testSortTArrayWithDecreaseOrderElements() {
+        String[] a =
+            {   "zack",   "zack", "stuart", "sheldon",
+             "sheldon", "rajesh",  "penny",    "lucy",
+             "leonard", "howard", "howard",  "eleven",
+              "eleven", "eleven",  "berny",     "amy"};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(objectAfterSorting[i], a[i]);
+        }
+    }
+    
+    /**
+     * Test method for {@link edu.nyu.cs.algorithm.sort.QuickSort#sort(T[])}.
+     */
+    @Test
+    public void testSortTArrayWithRandomOrderElements() {
+        String[] a = 
+            {"eleven", "leonard",  "penny", "stuart",
+             "rajesh",    "lucy",  "berny", "eleven",
+               "zack", "sheldon", "howard", "howard",
+                "amy", "sheldon",   "zack", "eleven"};
+        QuickSort.sort(a);
+        for (int i = 0; i < a.length; i++) {
+            assertEquals(objectAfterSorting[i], a[i]);
+        }
+    }
+
+}
